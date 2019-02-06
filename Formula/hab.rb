c2ccc3b1ca17271398dacefed03893aa5da1178f
Chef Expeditor <chef-ci@chef.io>
2019-02-06 21:19:02 +0000

    Bump version to 0.74.0
    Signed-off-by: Chef Expeditor <chef-ci@chef.io>

--- Formula/hab.rb
+++ Formula/hab.rb
@@ -1,8 +1,8 @@
 class Hab < Formula
   # Update these values as needed as new versions are released
-  current_version="0.73.0"
-  current_release="20190115004751"
-  current_sha256="3207e4369795f13b80088210ddfdb679be45351e1c70e5e872d0830c12fd6a76"
+  current_version="0.74.0"
+  current_release="20190204222313"
+  current_sha256="02d52c2f89b6fc29c13887491719b0708e60474eac7ac1d8623652dfe9732f67"
 
   # Installing "openssl" ensures there's a certificate chain at
   # `/usr/local/etc/openssl/cert.pem`, which our OpenSSL library needs
