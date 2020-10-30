7c1d7c6f2fd36b42dceff55da4eec1d79f2be557
Chef Expeditor <chef-ci@chef.io>
2020-10-30 20:01:09 +0000

    Bump version to 1.6.181
    Signed-off-by: Chef Expeditor <chef-ci@chef.io>

--- Formula/hab.rb
+++ Formula/hab.rb
@@ -1,7 +1,7 @@
 class Hab < Formula
   # Update these values as needed as new versions are released
-  current_version="1.6.175"
-  current_sha256="8cfc9cde5b801f7f5f15ec3d21b085df99082054f9abd616f31af1ac233528df"
+  current_version="1.6.181"
+  current_sha256="268c149431842a58cdbdcf3e7354f90dec38cd548568520b8a602e48d8d4cbb8"
   source_url = if current_version == "0.88.0"
     "https://bintray.com/habitat/stable/download_file?file_path=darwin%2Fx86_64%2Fhab-0.88.0-20191009205151-x86_64-darwin.zip"
   else
