de3459a4310c8b0c80d67551ac540300b097104f
Matt Wrock <matt@mattwrock.com>
2022-03-23 14:48:39 -0700

    Bump version to 1.6.477
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- Formula/hab.rb
+++ Formula/hab.rb
@@ -1,7 +1,7 @@
 class Hab < Formula
   # Update these values as needed as new versions are released
-  current_version="1.6.420"
-  current_sha256="1dd86170bbbc3a93ea7a88cb32f2b0e9f1ce30dffc9ef316f25103a7c497a5de"
+  current_version="1.6.477"
+  current_sha256="111251b15d7d3509008f953dd2a655491f645d7bebca7065db1b0c8755db57da"
   source_url = if current_version == "0.88.0"
     "https://bintray.com/habitat/stable/download_file?file_path=darwin%2Fx86_64%2Fhab-0.88.0-20191009205151-x86_64-darwin.zip"
   else
