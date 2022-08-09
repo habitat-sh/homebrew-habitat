a58ed35d221038b91ce65e5df668b44c5f91a8b3
Sheridan C Rawlins <41922797+scr-oath@users.noreply.github.com>
2022-08-09 11:12:06 -0700

    Update bump script.

--- .expeditor/scripts/bump_version.sh
+++ .expeditor/scripts/bump_version.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
+#
 # This script automates the modification of our Homebrew tap for the
 # `hab` binary.
 
 set -euo pipefail
+
+if [[ $(uname -s) == Darwin ]]; then
+  function sed() {
+      command gsed "$@"
+  }
+fi
 
 url_prefix="https://packages.chef.io/files/stable/habitat/latest/"
 
@@ -26,7 +32,8 @@ get_manifest() {
 }
 
 version="$(get_manifest | jq -r '.version')"
-sha256="$(curl "${url_prefix}/hab-x86_64-darwin.zip.sha256sum" | cut -d' ' -f1)"
+darwin_intel_sha256="$(curl "${url_prefix}/hab-x86_64-darwin.zip.sha256sum" | cut -d' ' -f1)"
+darwin_arm_sha256="$(curl "${url_prefix}/hab-aarch64-darwin.zip.sha256sum" | cut -d' ' -f1)"
 
 branch="ci/brew-update-$version-$(date +"%Y%m%d%H%M%S")"
 git checkout -b "$branch"
@@ -41,7 +48,11 @@ git add Formula/hab.rb
 
 sed --in-place \
     --regexp-extended \
-    's/current_sha256="(.*)"/current_sha256="'"${sha256}"'"/g' \
+    's/current_darwin_intel_sha256="(.*)"/current_darwin_intel_sha256="'"${darwin_intel_sha256}"'"/g' \
+    Formula/hab.rb
+sed --in-place \
+    --regexp-extended \
+    's/current_darwin_arm_sha256="(.*)"/current_darwin_arm_sha256="'"${darwin_arm_sha256}"'"/g' \
     Formula/hab.rb
 ensure_files_changed
 git add Formula/hab.rb
