cd2b8060ebe1c5ea1ea602fbc11e79ff1f05ff0c
Sheridan C Rawlins <41922797+scr-oath@users.noreply.github.com>
2022-08-09 10:58:18 -0700

    Update formula and bump script to support M1 macs (arm).

--- .expeditor/scripts/bump_version.sh
+++ .expeditor/scripts/bump_version.sh
@@ -26,7 +26,8 @@ get_manifest() {
 }
 
 version="$(get_manifest | jq -r '.version')"
-sha256="$(curl "${url_prefix}/hab-x86_64-darwin.zip.sha256sum" | cut -d' ' -f1)"
+darwin_intel_sha256="$(curl "${url_prefix}/hab-x86_64-darwin.zip.sha256sum" | cut -d' ' -f1)"
+darwin_arm_sha256="$(curl "${url_prefix}/hab-aarch64-darwin.zip.sha256sum" | cut -d' ' -f1)"
 
 branch="ci/brew-update-$version-$(date +"%Y%m%d%H%M%S")"
 git checkout -b "$branch"
@@ -41,7 +42,11 @@ git add Formula/hab.rb
 
 sed --in-place \
     --regexp-extended \
-    's/current_sha256="(.*)"/current_sha256="'"${sha256}"'"/g' \
+    's/current_darwin_intel_sha256="(.*)"/current_darwin_intel_sha256="'"${darwin_intel_sha256}"'"/g' \
+    Formula/hab.rb
+sed --in-place \
+    --regexp-extended \
+    's/current_darwin_arm_sha256="(.*)"/current_darwin_arm_sha256="'"${darwin_arm_sha256}"'"/g' \
     Formula/hab.rb
 ensure_files_changed
 git add Formula/hab.rb
--- Formula/hab.rb
+++ Formula/hab.rb
@@ -1,18 +1,33 @@
 class Hab < Formula
   # Update these values as needed as new versions are released
-  current_version="1.6.521"
-  current_sha256="7d83af8e267bed3865be3be102a21040fa03555dcf76f9d6f220dd66e614aa8c"
-  source_url = if current_version == "0.88.0"
-    "https://bintray.com/habitat/stable/download_file?file_path=darwin%2Fx86_64%2Fhab-0.88.0-20191009205151-x86_64-darwin.zip"
-  else
-    "https://packages.chef.io/files/habitat/#{current_version}/hab-x86_64-darwin.zip"
+  current_version = "1.6.521"
+  current_darwin_intel_sha256 = "7d83af8e267bed3865be3be102a21040fa03555dcf76f9d6f220dd66e614aa8c"
+  current_darwin_arm_sha256 = "7d83af8e267bed3865be3be102a21040fa03555dcf76f9d6f220dd66e614aa8c"
+
+  on_macos do
+    @source_os = "darwin"
+    if Hardware::CPU.arm?
+      @source_sha256 = current_darwin_arm_sha256
+      @source_arch = "aarch64"
+    elsif Hardware::CPU.intel?
+      @source_sha256 = current_darwin_intel_sha256
+      @source_arch = "x86_64"
+    end
   end
+  on_linux do
+    raise "linux not supported"
+  end
+  source_url = if current_version == "0.88.0"
+                 "https://bintray.com/habitat/stable/download_file?file_path=darwin%2Fx86_64%2Fhab-0.88.0-20191009205151-x86_64-darwin.zip"
+               else
+                 "https://packages.chef.io/files/habitat/#{current_version}/hab-#{@source_arch}-#{@source_os}.zip"
+               end
 
   desc "The Habitat command line application"
   homepage "https://habitat.sh"
   url source_url
   version current_version
-  sha256 current_sha256.downcase
+  sha256 @source_sha256
 
   def install
     bin.install "hab"
